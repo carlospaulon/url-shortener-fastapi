26c91c75f1e18acb068188a52d80f0d276f2e0b5
Carlos <paulonluis222@gmail.com>
2026-04-27 16:37:12 -0300

    feat: implement database persistence for URL creation

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,23 +1,55 @@
 import validators
-from fastapi import FastAPI, HTTPException
+import secrets
 
-from . import schemas
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine) # Crio o BD com as tabelas já modeladas
 
-@app.get('/')
-def read_root():
-    return 'Welcome to the URL Shortener APP :)'
+def get_db(): # Cada request cria uma sessão com o BD (é seguro?)
+    db = SessionLocal() # Crio uma sessão do BD
+    try: 
+        yield db # Retorna a conexão
+    finally:
+        db.close() # Fecha a conexão
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
 
+#Endpoints
+@app.get('/')
+def read_root():
+    return 'Welcome to the URL Shortener APP :)'
+
+
 # URL Post
-@app.post('/url')
-def create_url(url: schemas.URLBase):
+@app.post('/url', response_model=schemas.URLInfo) # Passamos como DTO do Post - URLInfo (herda tudo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)): # Função POST - Precisa de uma URL com base no Schema e DEPENDE de uma sessão com o BD para realizar a request
     if not validators.url(url.target_url): # Valida a URL baseada na URL do Schema (A URL com parâmetro)
         raise_bad_request(message='URL not valid')
-    return f'TODO: Create database entry for: {url.target_url}'
+    
+    # TODO: Lógica - Mudar lógica do short depois
+    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    key = ''.join(secrets.choice(chars) for _ in range(5))
+    secret_key = ''.join(secrets.choice(chars) for _ in range(8))
+
+    # Cria uma entrada do BD - 42 a 49
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    db_url.url = key
+    db_url.admin_url = secret_key
+    
+    
+    return db_url
 
 # KEY_URL Get
 

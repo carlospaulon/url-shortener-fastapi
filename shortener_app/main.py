21172ff8ecbd1ecca5c4d4512ee49f6db57bb782
Carlos <paulonluis222@gmail.com>
2026-04-29 19:12:47 -0300

    feat(api): implement URL shortening, redirect and admin endpoints

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,11 +1,14 @@
 import validators
-import secrets
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, crud
 from .database import SessionLocal, engine
+
+from starlette.datastructures import URL
+from .config import get_settings
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine) # Crio o BD com as tabelas já modeladas
@@ -17,8 +20,22 @@ def get_db(): # Cada request cria uma sessão com o BD (é seguro?)
     finally:
         db.close() # Fecha a conexão
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        'administration info', secret_key=db_url.secret_key
+    )
+
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
+def raise_not_found(request):
+    message = f'URL {request.url} not found'
+    raise HTTPException(status_code=404, detail=message)
 
 #Endpoints
 @app.get('/')
@@ -31,29 +48,45 @@ def read_root():
 def create_url(url: schemas.URLBase, db: Session = Depends(get_db)): # Função POST - Precisa de uma URL com base no Schema e DEPENDE de uma sessão com o BD para realizar a request
     if not validators.url(url.target_url): # Valida a URL baseada na URL do Schema (A URL com parâmetro)
         raise_bad_request(message='URL not valid')
-    
-    # TODO: Lógica - Mudar lógica do short depois
-    chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    key = ''.join(secrets.choice(chars) for _ in range(5))
-    secret_key = ''.join(secrets.choice(chars) for _ in range(8))
 
-    # Cria uma entrada do BD - 42 a 49
-    db_url = models.URL(
-        target_url=url.target_url, key=key, secret_key=secret_key
-    )
 
-    db.add(db_url)
-    db.commit()
-    db.refresh(db_url)
-    db_url.url = key
-    db_url.admin_url = secret_key
+    db_url = crud.create_db_url(db=db, url=url)
     
-    
-    return db_url
+    return get_admin_info(db_url)
+
 
 # KEY_URL Get
+@app.get('/{url_key}')
+def forward_to_target_url(
+    url_key: str,
+    request: Request,
+    db: Session = Depends(get_db)
+):
+
+    if db_url := crud.get_db_url_key(db=db, url_key=url_key):
+        crud.update_db_click(db=db, db_url=db_url) # Quando vou clicar no short link ele atualiza o click e redireciona para target
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
 
 # Admin KEY_URL Get
+@app.get('/admin/{secret_key}', name='administration info', response_model=schemas.URLInfo)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    
+    #walrus operator realiza a função/condicao e atribui a variável (verificamos basicamente se tem valor ou é None)
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url) # Pego as informações do admin do short link criado
+    else:
+        raise_not_found(request)
+    
 
 # Admin KEY_URL Delete
+@app.delete('/admin/{secret_key}')
+def delete_url(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.deactivate_db_url_by_secret_key(db=db, secret_key=secret_key):
+        message = f'Successfully deleted shortened URL for "{db_url.target_url}"'
+        return {'detail': message}
+    else:
+        raise_not_found(request)
 
